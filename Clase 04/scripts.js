62be08690210795adb4f0d26e4cd0f6f9dc24c90
ManuelCM21 <manuelchuncamamani@gmail.com>
2022-05-03 10:12:47 -0500

    Formulario con JavaScript

---
+++ Clase 04/scripts.js
@@ -0,0 +1,14 @@
+function par_impar() {
+    num1 = document.getElementById("n1").value;
+    console.log(num1);
+    if (num1.length>0 && !isNaN(num1)) {
+        if (num1%2==0) {
+            document.getElementById("rs").innerHTML="Respuesta: ES PAR";
+        } else {
+            document.getElementById("rs").innerHTML="Respuesta: ES IMPAR";
+        }
+    } else {
+        alert("Ingrese un numero");
+        document.getElementById("n1").focus();
+    }
+}
